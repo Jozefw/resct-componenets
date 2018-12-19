083baa12ab0269ff6575d52b57543fd6002e1a60
Jozef <jozefw@sbcglobal.net>
2018-12-18 16:30:00 -0800

    add children section

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
+const App = () =>{
+    return(
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail author='sam' time="4:55" comment="nice dude" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author='marge' time="6:05" comment="meh...I seen better" avatar={faker.image.avatar()}/>
+           </ApprovalCard>
+           <ApprovalCard>
+                <CommentDetail 
+                author='mary' 
+                time="11:11" 
+                comment="that's illegal"
+                avatar={faker.image.avatar()}/>
+           </ApprovalCard>
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
